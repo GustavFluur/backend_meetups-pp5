fafcb1cc553cbf6bca0553fa94a6d92d719c3a50
GustavFluur <gustavfluur@hotmail.com>
2022-12-15 08:07:07 +0000

    Add change back to orginal api

--- backend_sthlm/settings.py
+++ backend_sthlm/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-# api hasn't been created in the terminal
+# api hasn't been created in the terminal & test api =>
     'api',
 
     'rest_framework',
